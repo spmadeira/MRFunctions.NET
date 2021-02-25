a3d7ca35ab2a5557d111c191634c6cea06b51329
Samuel Madeira <spmadeira@edu.unifor.br>
2021-02-24 21:28:35 -0300

    Initial Commit

---
+++ MRBuilder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MRFunctions
+{
+    public class MRBuilder<TInput>
+    {
+        public MRBuilder<TInput, TData> WithReader<TData>(Func<TInput, IEnumerable<TData>> readFunction)
+        {
+            return new()
+            {
+                Read = readFunction
+            };
+        }
+    }
+
+    public class MRBuilder<TInput, TData> : MRBuilder<TInput>
+    {
+        public Func<TInput, IEnumerable<TData>> Read { get; init; }
+
+        public MRBuilder<TInput, TData, TKey, TValue> WithMapper<TKey, TValue>(
+            Func<TData, IEnumerable<KeyValuePair<TKey, TValue>>> mapper)
+        {
+            return new()
+            {
+                Read = Read,
+                Map = mapper
+            };
+        }
+        
+    }
+
+    public class MRBuilder<TInput, TData, TKey, TValue> : MRBuilder<TInput, TData>
+    {
+        internal Func<TData, IEnumerable<KeyValuePair<TKey, TValue>>> Map { get; init; }
+        
+        internal Func<TKey, TKey, bool> Compare { get; set; } = (k1, k2) => k1.Equals(k2);
+        
+        internal Func<TKey, IEnumerable<TValue>, TValue> Reduce { get; set; }
+
+        internal Func<KeyValuePair<TKey, TValue>, Task> Write { get; set; }
+            = async (pair) => Console.WriteLine($"Key: {pair.Key} | Value: {pair.Value}");
+
+        public MRBuilder<TInput, TData, TKey, TValue> WithComparer(Func<TKey, TKey, bool> comparer)
+        {
+            Compare = comparer;
+            return this;
+        }
+        
+        public MRBuilder<TInput, TData, TKey, TValue> WithReducer(Func<TKey, IEnumerable<TValue>, TValue> reducer)
+        {
+            Reduce = reducer;
+            return this;
+        }
+
+        public MRBuilder<TInput, TData, TKey, TValue> WithWriter(Func<KeyValuePair<TKey, TValue>, Task> writer)
+        {
+            Write = writer;
+            return this;
+        }
+        
+        public MRBuilder<TInput, TData, TKey, TValue> WithWriter(Action<KeyValuePair<TKey, TValue>> writer)
+        {
+            Write = pair => Task.Run(() => writer(pair));
+            return this;
+        }
+
+        public MapReduce<TInput, TData, TKey, TValue> Build()
+        {
+            return new()
+            {
+                Read = Read,
+                Map = Map,
+                Compare = Compare,
+                Reduce = Reduce,
+                Write = Write
+            };
+        }
+
+        public static implicit operator MapReduce<TInput, TData, TKey, TValue>(MRBuilder<TInput, TData, TKey, TValue> builder)
+        {
+            return builder.Build();
+        }
+    }
+}
---
+++ MapReduce.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MRFunctions
+{
+    public abstract class MapReduce
+    {
+        public static MRBuilder<TInput> WithInput<TInput>()
+        {
+            return new();
+        }
+    }
+
+    public class MapReduce<
+        TInput,
+        TData,
+        TKey,
+        TValue>
+    {
+        internal Func<TInput, IEnumerable<TData>> Read { get; init; }
+        internal Func<TData, IEnumerable<KeyValuePair<TKey, TValue>>> Map { get; init; }
+        internal Func<TKey, TKey, bool> Compare { get; init; }
+        internal Func<TKey, IEnumerable<TValue>, TValue> Reduce { get; init; }
+        internal Func<KeyValuePair<TKey, TValue>, Task> Write { get; init; }
+
+        private IEnumerable<TData> Data;
+        private ConcurrentBag<IEnumerable<KeyValuePair<TKey, TValue>>> Groups;
+        private ConcurrentBag<KeyValuePair<TKey, List<TValue>>> Buckets;
+        private ConcurrentBag<KeyValuePair<TKey, TValue>> Pairs;
+
+        public async Task Run(TInput input)
+        {
+            Data = await Task.Run(() => Read(input));
+            
+            Groups = new ConcurrentBag<IEnumerable<KeyValuePair<TKey, TValue>>>();
+
+            var mapOperations = Data
+                .Select(MapData);
+
+            await Task.WhenAll(mapOperations);
+
+            Buckets = new ConcurrentBag<KeyValuePair<TKey, List<TValue>>>();
+            
+            var shuffleOperations = Groups
+                .Select(Shuffle);
+
+            await Task.WhenAll(shuffleOperations);
+
+            Pairs = new ConcurrentBag<KeyValuePair<TKey, TValue>>();
+
+            var reductionOperations = Buckets
+                .Select(ReduceBucket);
+
+            await Task.WhenAll(reductionOperations);
+
+            var writeOperations = Pairs
+                .Select(Write);
+
+            await Task.WhenAll(writeOperations);
+        }
+
+        private Task MapData(TData data)
+        {
+            return Task.Run(() =>
+            {
+                var group = Map(data);
+                Groups.Add(group);
+            });
+        }
+        
+        private Task Shuffle(IEnumerable<KeyValuePair<TKey, TValue>> group)
+        {
+            var tasks = group.Select(pair =>
+            {
+                return Task.Run(() =>
+                {
+                    foreach (var bucket in Buckets)
+                    {
+                        if (Compare(bucket.Key, pair.Key))
+                        {
+                            bucket.Value.Add(pair.Value);
+                            return;
+                        }
+                    }
+
+                    Buckets.Add(new KeyValuePair<TKey, List<TValue>>(pair.Key, new List<TValue> {pair.Value}));
+                });
+            });
+
+            return Task.WhenAll(tasks);
+        }
+
+        private Task ReduceBucket(KeyValuePair<TKey, List<TValue>> bucket)
+        {
+            return Task.Run(() =>
+            {
+                var value = Reduce(bucket.Key, bucket.Value.ToArray());
+                Pairs.Add(new KeyValuePair<TKey, TValue>(bucket.Key, value));
+            });
+        }
+    }
+}
